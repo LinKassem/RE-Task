67ba4b48dd5409c0cf9e91b74ba9202bb67378db
Lin Kassem <linsara1@gmail.com>
2017-12-18 23:33:59 +0200

    added js for search form submit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
 var key = '3fb5e4f43161429e6ff05772449416ef';
-var tags = 'sea';
+var queryTags = 'sea,birds';
 var httpRequest = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
-var url = httpRequest + '&api_key=' + key + '&format=json&nojsoncallback=1' + '&tags=' + tags + '&extras=url_s' ;
+var url;
 
-$( document ).ready(function() {
+function flickerAPI(key, httpRequest, queryTags) {
+
+	url = httpRequest + '&api_key=' + key + '&format=json&nojsoncallback=1' + '&tags=' + queryTags + '&extras=url_s' ;
 
 	$.get( url, function( data ) {
-
 		for (var i = data.photos.photo.length - 1; i >= 0; i--) {
-			var imageUrl = data.photos.photo[i].url_s;
-			$('.main-content').append( '<div class="image-container"><a><img data-src="' + imageUrl + '" ></a></div>');
+			$('.main-content').append( '<div class="image-container"><a><img data-src="'
+				+ data.photos.photo[i].url_s + '" ></a></div>');
+		}
+
+	});
+}
+
+// executed when the HTML document is loaded and the DOM is ready
+$( document ).ready(function() {
+
+	// trigger the flickerAPI so that initially there is content on the page
+	flickerAPI(key, httpRequest, queryTags);
+
+	// listen for form submission events
+	$( '.search__form' ).submit(function( event ) {
+		queryTags = $( this ).find( "input[name='q']" ).val();
+		if(queryTags.replace(/\s/g, '').length != 0 ) {
+			queryTags = queryTags.split(/[ ,]+/).join(',')
+			$('main-content').empty();
+			flickerAPI(key, httpRequest, queryTags);
+		} else {
+			event.preventDefault();
 		}
 
 	});
 
-	/*  initialize all elements matching img[data-src] selector to be lazy loaded */
-	$(window).on('ajaxComplete', function() {
-		setTimeout(function() {
-			$(window).lazyLoadXT();
-		}, 50);
-	});
+});
 
+// executed when the page is fully loaded with the all
+// the images that resulted from the initial flickerAPI usage
+$(window).on('ajaxComplete', function() {
+	setTimeout(function() {
+		$(window).lazyLoadXT();
+	}, 50);
 });
