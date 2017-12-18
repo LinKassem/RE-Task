5c2ff4b4308395e4cdc72344c39c3673dd43abae
Lin Kassem <linsara1@gmail.com>
2017-12-18 19:04:00 +0200

    working flicker API + fading lazyloading

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 var key = '3fb5e4f43161429e6ff05772449416ef';
-var tags = 'cars,movie';
+var tags = 'cars,cartoon';
 var httpRequest = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
 var url = httpRequest + '&api_key=' + key + '&format=json&nojsoncallback=1' + '&tags=' + tags + '&extras=url_s' ;
 
+
+
 $( document ).ready(function() {
 	$.get( url, function( data ) {
+
 		console.log(data.photos.photo.length);
+
+		/*  initialize all elements matching img[data-src] selector */
+		$(window).lazyLoadXT();
 		for (var i = data.photos.photo.length - 1; i >= 0; i--) {
-			$('.main-content').append( '<div class="image"><a><img src="' + data.photos.photo[i].url_s + '" ></a></div>');
+			$('.main-content').append( '<div><a><img data-src="' + data.photos.photo[i].url_s + '" ></a></div>');
 		}
+
 	});
+
+    $(window).on('ajaxComplete', function() {
+        setTimeout(function() {
+            $(window).lazyLoadXT();
+        }, 50);
+    });
+
 });
+
+
