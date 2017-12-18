2fe165ee7a48f34aa5faa8befc0faa38a7eb3305
Lin Kassem <linsara1@gmail.com>
2017-12-18 22:10:44 +0200

    working lazyloading

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
 var key = '3fb5e4f43161429e6ff05772449416ef';
-var tags = 'cars,cartoon';
+var tags = 'sea';
 var httpRequest = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
 var url = httpRequest + '&api_key=' + key + '&format=json&nojsoncallback=1' + '&tags=' + tags + '&extras=url_s' ;
 
-
-
 $( document ).ready(function() {
+
 	$.get( url, function( data ) {
 
-		console.log(data.photos.photo.length);
-
-		/*  initialize all elements matching img[data-src] selector */
-		$(window).lazyLoadXT();
 		for (var i = data.photos.photo.length - 1; i >= 0; i--) {
-			$('.main-content').append( '<div><a><img data-src="' + data.photos.photo[i].url_s + '" ></a></div>');
+			var imageUrl = data.photos.photo[i].url_s;
+			$('.main-content').append( '<div class="image-container"><a><img data-src="' + imageUrl + '" ></a></div>');
 		}
 
 	});
 
-    $(window).on('ajaxComplete', function() {
-        setTimeout(function() {
-            $(window).lazyLoadXT();
-        }, 50);
-    });
+	/*  initialize all elements matching img[data-src] selector to be lazy loaded */
+	$(window).on('ajaxComplete', function() {
+		setTimeout(function() {
+			$(window).lazyLoadXT();
+		}, 50);
+	});
 
 });
-
-
