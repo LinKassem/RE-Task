43bb4c7a0b758b3cee6ba2d3743614bc6c53800c
Lin Kassem <linsara1@gmail.com>
2017-12-20 23:28:14 +0200

    cleaned up js

--- js/main.js
+++ js/main.js
@@ -3,45 +3,54 @@ var queryTags = 'sea,birds';
 var httpRequest = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
 var url;
 
+/**
+ * Triggert the FlickerAPI image search
+ * @param  {String} key         [Flicker API key]
+ * @param  {String} httpRequest [Base HTTP GET request]
+ * @param  {String} queryTags   [Comma separated words that the user wants to search for]
+ * @return none
+ */
 function flickerAPI(key, httpRequest, queryTags) {
 
-
 	url = httpRequest + '&api_key=' + key + '&format=json&nojsoncallback=1' + '&tags=' + queryTags + '&extras=url_s' ;
-	var html = '';
-	var html2 = '';
-	console.log(url);
+	var imageId = 0;
+	var imageContainer = '';
+	var imageInfoContainer = '';
+	// console.log(url);
 	$.get( url, function( data ) {
-		for (var i = data.photos.photo.length - 1; i >= 0; i--) {
-			html = '<div class="image-container is-collapsed"' +' id=' + i +'>' +
+		for (var i = 0; i <= data.photos.photo.length - 1; i++) {
+			imageId = data.photos.photo[i].id;
+			imageContainer = '<div class="image-container is-collapsed">' +
 						'<div class="image--normal">' +
 							'<a href="#!">' +
-								'<img data-src="'+ data.photos.photo[i].url_s + '" data-id="#' + data.photos.photo[i].id + '" >' +
+								'<img data-src="'+ data.photos.photo[i].url_s + '" data-id="#' + imageId + '" >' +
 							'</a>' +
 						'</div>' +
 					'</div>' ;
 
-			html2 += '<div class="image-info-container" id="'+ data.photos.photo[i].id +'"><div>Description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati veniam aliquam eos eius blanditiis, facilis minus quod nostrum. Dolores recusandae doloremque quam consequatur consequuntur accusantium quos possimus inventore ratione reiciendis!</div></div>'
-			$('.main-content').append( html);
+			imageInfoContainer += '<div class="image-info-container" id="'+ imageId +'">'+
+									'<div>'+
+										'Description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati veniam aliquam eos eius blanditiis, facilis minus quod nostrum. Dolores recusandae doloremque quam consequatur consequuntur accusantium quos possimus inventore ratione reiciendis!' +
+									'</div>' +
+								'</div>'
 
+
+			$('.main-content').append( imageContainer );
 
 		}
-		$('.main-content').append(html2);
+
+		$('.main-content').append(imageInfoContainer);
+
 	});
 }
 
-
-
-
-/*
-get the last element in the current row
-so that we can place the expanded image after it in the dom
- */
 /**
  * Get the last sibling in the row where the passed element lies.
- * @param  {DOM object} element [element I want to find it's sibling that appears last in the row]
- * @return {DOM object}        	[element's sibling that appears last in the row]
+ * @param  {DOM object} 		[Element I want to find it's sibling that appears last in the row]
+ * @return {DOM object}        	[Element's sibling that appears last in the row]
  */
 function getLastElementInCurrentRow(element) {
+
 	var bottomOffsetOfelement = element.getBoundingClientRect().bottom;
 
 	for (var i = 1; i < $(element).siblings().length; i++) {
@@ -53,10 +62,18 @@ function getLastElementInCurrentRow(element) {
 	return element;
 }
 
+/**
+ * Expand the side navigation in mobile view
+ * @return {none}
+ */
 function openSideNav() {
 	$('.side-nav').css('width', '250px');
 }
 
+/**
+ * Closes the side navigation in mobile view
+ * @return {none}
+ */
 function closeSideNav() {
 	$('.side-nav').css('width', '0');
 }
@@ -100,24 +117,18 @@ $(document).on('click', '.image-container.is-collapsed', function() {
 
 	// get the last element in the row after the clicked image
 	var lastRowElement = getLastElementInCurrentRow(this);
-	// get the clicked image href tag, which represents the image id
+
+	// get the clicked image data-id attribute value
 	var id = $(this).find('img').attr('data-id');
-	// insert the div with the same id as the href after the lastRowElement
-	$( id ).insertAfter( $(lastRowElement) );
+
+	// insert the div with the same id as the image data-src value after the lastRowElement
+	$(id).insertAfter( $(lastRowElement) );
+
 	$(id).addClass('is-expanded');
 
-	// if (document.getElementById(id.substr(1)).style.maxHeight){
- //    	document.getElementById(id.substr(1)).style.maxHeight = null;
- //    } else {
- //    	document.getElementById(id.substr(1)).style.maxHeight = document.getElementById(id.substr(1)).scrollHeight + "px";
- //    }
- //
-	if ($(id).css('max-height') != '0px'){
-    	$(id).css('max-height', '0');
-    } else {
-    	$(id).css('max-height', $(id).prop("scrollHeight"));
-    	// document.getElementById(id.substr(1)).style.maxHeight = document.getElementById(id.substr(1)).scrollHeight + "px";
-    }
+	// get the height of the div and set it to the max-height property.
+	// This is necessary for div expansion transition effect to work.
+	$(id).css('max-height', $(id).prop("scrollHeight"));
 
 	$(this).removeClass('is-collapsed').addClass('is-expanded');
 
